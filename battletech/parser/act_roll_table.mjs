fa7ddd6d93bb86f916d4a0a6d7f47b3a1f1836b6
Sheep-y <your.sheepy@gmail.com>
2018-06-26 17:37:25 +0800

    Add weighted roll formula for prototyping Attack Improvement Mod.

--- battletech/parser/act_roll_table.mjs
+++ battletech/parser/act_roll_table.mjs
@@ -2,15 +2,35 @@
 
 import { td, tdr, tdh, newRow, d2, sum, log } from './bt_utils.mjs';
 
-const rollList = [ 100, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35, 30, 25, 20, 17, 15, 12, 10, 9, 8, 7, 6, 5, 4, 3, 2.4, 2, 1, 0 ];
+const rollList = [ 100, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35, 30, 25, 20, 18, 16, 14, 12, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 ];
 const modified = rollList.reduce( ( v, e ) => { v[ e ] = reverse_correction( e ); return v; }, {} );
 const bonusList = [];
 
-function reverse_correction( target ) {
-   // Solving r for corected = ((1.6r-0.8)^3+0.5+r)/2 where r = RNG roll
+function correction( roll ) {
+   return (Math.pow(1.6*roll-0.8,3)+0.5+roll)/2;
+}
+
+function correction2( roll, strength ) {
+   strength /= 2;
+   const revStr = 1-strength;
+   return (Math.pow(1.6*roll-0.8,3)+0.5)*strength + roll*revStr;
+}
+
+function reverse_correction( target, strength ) {
+   // Solving r for target = ((1.6r-0.8)^3+0.5+r)/2 where r = RNG roll, BattleTech.AttackDirector.AttackSequence.GetCorrectedRoll
    const e = target/100, a = Math.pow( 125*Math.sqrt( 13824*e*e - 13824*e + 3581 ) / ( 4096*Math.pow( 6, 3/2 ) ) + ( 250*e - 125 )/1024, 1/3 );
    return a - 125/(1536*a) + 0.5;
 }
+
+function reverse_correction2( target, strength ) {
+   // Solving r for target = ((1.6r-0.8)^3+0.5)*(s/2)+r*(1-s/2)
+   if ( strength === 0 ) return target;
+   const t = target, t2 = t*t, s = strength, s2 = s*s, s3 = s2*s;
+   const a = 125 * Math.sqrt( ( 13824*t2*s - 13824*t*s - 125*s3 + 750*s2 + 1956*s + 1000 ) / s );
+   const b = Math.pow( a / ( 4096*Math.pow(6,3/2)*s ) + ( 250*t - 125 ) / ( 1024 * s ), 1/3 );
+   return b + (125*s-250)/(1536*s*b) + 0.5;
+}
+
 
 log( "|* Shown |* Real |* x2     |* x6     |* x20    |" );
 for ( const roll of rollList ) {
@@ -32,6 +52,7 @@ function perc( val ) {
    return d2( val * 100 ) + "%";
 }
 
+// Miss breaker simulator - BattleTech.Team.ProcessRandomRoll
 function recur( levelLeft, accuracy, stack ) { 'use strict'; // Make sure tail call can be optimised
    if ( levelLeft <= 0 ) {
       bonusList.length = 0;
@@ -64,5 +85,88 @@ function recur( levelLeft, accuracy, stack ) { 'use strict'; // Make sure tail c
    return recur( levelLeft-1, accuracy, stack );
 }
 
+const FrontHitTable = { "Head": 1, "CenterTorso": 16, "LeftTorso": 14, "RightTorso": 14, "LeftArm": 10, "RightArm": 10, "LeftLeg": 8, "RightLeg": 8 };
+const FrontHeadTable = { "Head": 1, "CenterTorso": 16*4, "LeftTorso": 14*4, "RightTorso": 14*4, "LeftArm": 10, "RightArm": 10, "LeftLeg": 8, "RightLeg": 8 };
+
+function GetHitLocationOrig ( hitTable, /*float*/ roll, bonusLocation, /*float*/ bonusMultiplier) {
+   /* int */ let weightSum = 0, i = 0;
+   for ( const [ location, weight ] of Object.entries( hitTable ) ) {
+      if ( location === bonusLocation )
+         weightSum += /* (int) float */ parseInt( weight * bonusMultiplier ); 
+      else
+         weightSum += /* int */ weight;
+      log( `${location} bracket: ${weightSum}` );
+   }
+   const target = /* (int) float */ parseInt( roll * weightSum );
+   for ( const [ location, weight ] of Object.entries( hitTable ) ) {
+      if ( ! weight ) continue; // Skip locations that are impossible to hit
+      if ( location === bonusLocation )
+         i += /* (int) float */ parseInt( weight * bonusMultiplier );
+      else
+         i += weight;
+      log( `${location} i: ${i}` );
+      if ( i >= target ) {
+         log( "Hit" );
+         return location;
+      }
+   }
+   return "None";
+}
+
+function ReverseHitLocation ( hitTable, bonusLocation, bonusMultiplier ) {
+   let totalWeight = 0;
+   for ( const weight of hitTable ) totalWeight += weight;
+   if ( bonusLocation ) totalWeight += ( bonusMultiplier - 1 ) * hitTable[ bonusLocation ];
+
+   const percPerWeight = 100/totalWeight;
+   
+   
+}
+
+function GetHitLocation ( hitTable, /*float*/ roll, bonusLocation, /*float*/ bonusMultiplier) {
+   const scale = 1024;
+   /* int */ let weightSum = 0, i = 0;
+   for ( const [ location, weight ] of Object.entries( hitTable ) ) {
+      if ( location === bonusLocation )
+         weightSum += /* (int) float */ parseInt( weight * bonusMultiplier * scale ); 
+      else
+         weightSum += /* int */ weight * scale;
+   }
+   const target = /* (int) float */ parseInt( roll * weightSum );
+   for ( const [ location, weight ] of Object.entries( hitTable ) ) {
+      if ( ! weight ) continue; // Skip locations that are impossible to hit
+      if ( location === bonusLocation )
+         i += /* (int) float */ parseInt( weight * bonusMultiplier * scale );
+      else
+         i += weight * scale;
+      if ( i >= target )
+         return location;
+   }
+   return "None";
+}
 
 
+const data = { ... FrontHitTable };
+//GetHitLocation( FrontHitTable, 0.02469, "", 0 );
+
+/**
+for ( const key in data ) data[ key ] = 0;
+for ( let i = 0 ; i < 1000000 ; i++ )
+   data[ GetHitLocation( FrontHitTable, i/1000000, "", 0 ) ]++;
+log( data );
+
+for ( const key in data ) data[ key ] = 0;
+for ( let i = 0 ; i < 1000000 ; i++ )
+   data[ GetHitLocation( FrontHeadTable, i/1000000, "Head", 2 ) ]++;
+log( data );
+
+for ( const key in data ) data[ key ] = 0;
+for ( let i = 0 ; i < 1000000 ; i++ )
+   data[ GetHitLocation( FrontHeadTable, i/1000000, "Head", 4.75 ) ]++;
+log( data );
+
+for ( const key in data ) data[ key ] = 0;
+for ( let i = 0 ; i < 1000000 ; i++ )
+   data[ GetHitLocation( FrontHeadTable, i/1000000, "Head", 18 ) ]++;
+log( data );
+*/
